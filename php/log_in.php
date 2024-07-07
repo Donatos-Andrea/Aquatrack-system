6354f25a3f5c2ef9fa06d93f02a09a24a5f616ee
Sofia <sabarrantes2911@gmail.com>
2024-07-08 06:27:07 +0800

    add changes

--- php/log_in.php
+++ php/log_in.php
@@ -19,13 +19,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $sql = "SELECT * FROM admin WHERE Email = '$email' AND Password = '$password'";
     $result_user = mysqli_query($conn, $sql);
-    
+
     if (mysqli_num_rows($result_user) == 1) {
         $row = mysqli_fetch_assoc($result_user);
         session_start();
         $_SESSION['admin_id'] = $row['admin_id'];
         $_SESSION['Email'] = $email;
-    
+
         header('Location: ../home.html');
         exit();
     } else {
