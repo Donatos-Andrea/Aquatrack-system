6f38a7bbd60998d1b8121c334e1212a86338e8ac
Andreadonatos <dreadonatos@gmail.com>
2024-07-08 11:26:43 +0800

    add changes

--- php/customer.php
+++ php/customer.php
@@ -11,6 +11,8 @@ if (!$conn) {
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $customerId = $_POST['customerid'];
     $firstName = $_POST['firstname'];
     $middleName = $_POST['middlename'];
     $lastName = $_POST['lastname'];
@@ -21,12 +23,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $contactNo = $_POST['number'];
     $email = $_POST['email'];
 
-    if (empty($firstName) || empty($middleName) || empty($lastName) || empty($municipality) || empty($barangay) || empty($zipCode) || empty($street) || empty($contactNo) || empty($email)) {
+    if (empty($customerId) || empty($firstName) || empty($middleName) || empty($lastName) || empty($municipality) || empty($barangay) || empty($zipCode) || empty($street) || empty($contactNo) || empty($email)) {
         die("All fields are required.");
     }
 
-    $sql = "INSERT INTO `customer` (`lastname`, `firstname`, `middlename`, `street`, `barangay`, `municipality`, `zip_code`, `contact_number`, `email_address`)
-            VALUES ('$lastName', '$firstName', '$middleName', '$street', '$barangay', '$municipality', '$zipCode', '$contactNo', '$email')";
+    $sql = "INSERT INTO `customer` (`customerid`, `firstname`, `middlename`, `lastname`, `street`, `barangay`, `municipality`, `zip_code`, `contact_number`, `email_address`)
+            VALUES ('$customerId','$firstName', '$middleName', '$lastName', '$street', '$barangay', '$municipality', '$zipCode', '$contactNo', '$email')";
 
     if (mysqli_query($conn, $sql)) {
         echo "Customer Information successfully added!";
